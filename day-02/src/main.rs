64ab08d6685444a50b3e6f04dc93395c2ae61881
Jake Goulding <jake.goulding@gmail.com>
2024-12-02 10:09:07 -0500

    Day 02 - Part 2

--- day-02/src/main.rs
+++ day-02/src/main.rs
@@ -4,28 +4,50 @@ const INPUT: &str = include_str!("../input.txt");
 
 fn main() {
     assert_eq!(606, n_safe(INPUT));
+    assert_eq!(644, n_safe_dampened(INPUT));
 }
 
 fn n_safe(s: &str) -> usize {
+    s.lines().filter(|l| levels_are_safe(parse(l))).count()
+}
+
+fn n_safe_dampened(s: &str) -> usize {
     s.lines()
         .filter(|l| {
-            let levels = l
-                .split_ascii_whitespace()
-                .map(|n| n.parse::<i64>().expect("number was malformed"));
+            let levels = parse(l).collect::<Vec<_>>();
 
-            let mut diffs = levels.tuple_windows().map(|(l, r)| l - r);
-
-            let in_range = |v: i64| (1..=3).contains(&v.abs());
-
-            let first = diffs.next().expect("Did not have at least two numbers");
-
-            if !in_range(first) {
-                return false;
+            if levels_are_safe(levels.iter().copied()) {
+                return true;
             }
 
-            diffs.all(|l| first.signum() == l.signum() && in_range(l))
+            (0..levels.len()).any(|to_drop| {
+                let dropped_one = levels
+                    .iter()
+                    .enumerate()
+                    .filter_map(|(i, &l)| (i != to_drop).then_some(l));
+                levels_are_safe(dropped_one)
+            })
         })
         .count()
+}
+
+fn parse(s: &str) -> impl Iterator<Item = i64> {
+    s.split_ascii_whitespace()
+        .map(|n| n.parse::<i64>().expect("number was malformed"))
+}
+
+fn levels_are_safe(levels: impl Iterator<Item = i64>) -> bool {
+    let mut diffs = levels.tuple_windows().map(|(l, r)| l - r);
+
+    let in_range = |v: i64| (1..=3).contains(&v.abs());
+
+    let first = diffs.next().expect("Did not have at least two numbers");
+
+    if !in_range(first) {
+        return false;
+    }
+
+    diffs.all(|l| first.signum() == l.signum() && in_range(l))
 }
 
 #[cfg(test)]
@@ -37,5 +59,10 @@ mod test {
     #[test]
     fn example() {
         assert_eq!(2, n_safe(EXAMPLE));
+    }
+
+    #[test]
+    fn example_dampened() {
+        assert_eq!(4, n_safe_dampened(EXAMPLE));
     }
 }
