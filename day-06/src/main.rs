263752e5c5b2ba695d90fec82b804d1e0979a14d
Jake Goulding <jake.goulding@gmail.com>
2024-12-06 10:53:13 -0500

    Day 06 - Part 2

--- day-06/src/main.rs
+++ day-06/src/main.rs
@@ -1,9 +1,19 @@
-use std::collections::BTreeSet;
+use bitflags::bitflags;
+use std::collections::{BTreeMap, BTreeSet};
 
 const INPUT: &str = include_str!("../input.txt");
 
 fn main() {
     assert_eq!(5239, distinct_guard_positions(INPUT));
+
+    let part_2 = new_obstruction_positions(INPUT);
+
+    assert!(part_2 < 1932);
+    // Forgot to check that we _could_ put the obstacle in the
+    // upcoming square (i.e. that we hadn't previously walked through
+    // the square).
+
+    assert_eq!(1753, part_2);
 }
 
 fn distinct_guard_positions(s: &str) -> usize {
@@ -30,6 +40,66 @@ fn distinct_guard_positions(s: &str) -> usize {
     }
 
     visited.len()
+}
+
+fn new_obstruction_positions(s: &str) -> usize {
+    use Direction::*;
+
+    let (mut grid, max, mut guard) = parse(s);
+
+    let mut direction = U;
+    let mut visited = BTreeSet::new();
+    let mut possible_loops = 0;
+
+    loop {
+        visited.insert(guard);
+
+        let Some(next) = step(guard, direction, max) else {
+            break;
+        };
+
+        if grid.contains(&next) {
+            direction = direction.turn();
+            continue;
+        } else if !visited.contains(&next) {
+            // But what if there _was_ an obstacle?
+            grid.insert(next);
+            if is_loop(&grid, guard, direction, max) {
+                possible_loops += 1;
+            }
+            grid.remove(&next);
+        }
+
+        guard = next;
+    }
+
+    possible_loops
+}
+
+fn is_loop(grid: &Grid, mut guard: Coord, mut direction: Direction, max: Max) -> bool {
+    let mut visited = BTreeMap::new();
+
+    loop {
+        let footprint = direction.to_footprint();
+        let square = visited.entry(guard).or_insert_with(Footprint::empty);
+
+        if square.contains(footprint) {
+            return true;
+        }
+
+        square.insert(footprint);
+
+        let Some(next) = step(guard, direction, max) else {
+            return false;
+        };
+
+        if grid.contains(&next) {
+            direction = direction.turn();
+            continue;
+        }
+
+        guard = next;
+    }
 }
 
 type Grid = BTreeSet<Coord>;
@@ -91,6 +161,17 @@ impl Direction {
             L => U,
         }
     }
+
+    fn to_footprint(self) -> Footprint {
+        use Direction::*;
+
+        match self {
+            U => Footprint::U,
+            R => Footprint::R,
+            D => Footprint::D,
+            L => Footprint::L,
+        }
+    }
 }
 
 fn step(coord: Coord, direction: Direction, max: Max) -> Option<Coord> {
@@ -132,6 +213,16 @@ fn step(coord: Coord, direction: Direction, max: Max) -> Option<Coord> {
     Some(next)
 }
 
+bitflags! {
+    #[derive(Copy, Clone)]
+    struct Footprint: u8 {
+        const U = 0b0001;
+        const R = 0b0010;
+        const D = 0b0100;
+        const L = 0b1000;
+    }
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -141,5 +232,10 @@ mod test {
     #[test]
     fn example() {
         assert_eq!(41, distinct_guard_positions(EXAMPLE));
+    }
+
+    #[test]
+    fn example_positions() {
+        assert_eq!(6, new_obstruction_positions(EXAMPLE));
     }
 }
