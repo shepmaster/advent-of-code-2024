a187d2691dad340432cafa09fdf2d61895a31107
Jake Goulding <jake.goulding@gmail.com>
2024-12-07 15:21:44 -0500

    Day 07 - Part 2

--- day-07/src/main.rs
+++ day-07/src/main.rs
@@ -5,30 +5,21 @@ const INPUT: &str = include_str!("../input.txt");
 
 fn main() {
     assert_eq!(1582598718861, total_calibration(INPUT));
+    assert_eq!(165278151522644, total_calibration_concat(INPUT));
 }
 
 fn total_calibration(s: &str) -> u64 {
+    total_calibration_with_operators(s, &Op::LIMITED)
+}
+
+fn total_calibration_concat(s: &str) -> u64 {
+    total_calibration_with_operators(s, &Op::ALL)
+}
+
+fn total_calibration_with_operators(s: &str, op_choices: &[Op]) -> u64 {
     s.lines()
         .map(parse_line)
-        .filter(|&(test, ref numbers)| {
-            let (&head, tail) = numbers.split_first().expect("Need more than one number");
-
-            let ops = [Op::Add, Op::Mul];
-
-            iter::repeat_n(&ops, tail.len())
-                .multi_cartesian_product()
-                .any(|ops| {
-                    let computed = ops
-                        .into_iter()
-                        .zip(tail)
-                        .fold(head, |acc, (op, &n)| match op {
-                            Op::Add => acc + n,
-                            Op::Mul => acc * n,
-                        });
-
-                    test == computed
-                })
-        })
+        .filter(|(test, numbers)| test_with_operators(numbers, op_choices, *test))
         .map(|(test, _)| test)
         .sum()
 }
@@ -45,10 +36,43 @@ fn parse_line(l: &str) -> (u64, Vec<u64>) {
     (test, numbers)
 }
 
+fn test_with_operators(numbers: &[u64], op_choices: &[Op], test: u64) -> bool {
+    let (&head, tail) = numbers.split_first().expect("Need more than one number");
+
+    iter::repeat_n(op_choices, tail.len())
+        .multi_cartesian_product()
+        .any(|ops| {
+            let computed = ops
+                .into_iter()
+                .zip(tail)
+                .fold(head, |acc, (op, &n)| op.apply(acc, n));
+
+            test == computed
+        })
+}
+
 #[derive(Debug, Copy, Clone)]
 enum Op {
     Add,
     Mul,
+    Concat,
+}
+
+impl Op {
+    const ALL: [Self; 3] = [Op::Add, Op::Mul, Op::Concat];
+    const LIMITED: [Self; 2] = [Op::Add, Op::Mul];
+
+    fn apply(self, l: u64, r: u64) -> u64 {
+        match self {
+            Op::Add => l + r,
+            Op::Mul => l * r,
+            Op::Concat => {
+                let n_r_digits = r.ilog10() + 1;
+                let f = 10u64.pow(n_r_digits);
+                l * f + r
+            }
+        }
+    }
 }
 
 #[cfg(test)]
@@ -60,5 +84,10 @@ mod test {
     #[test]
     fn example() {
         assert_eq!(3749, total_calibration(EXAMPLE));
+    }
+
+    #[test]
+    fn example_concat() {
+        assert_eq!(11387, total_calibration_concat(EXAMPLE));
     }
 }
