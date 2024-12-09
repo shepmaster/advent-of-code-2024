d975186a1e08056480f0bd6004147d7c6d109195
Jake Goulding <jake.goulding@gmail.com>
2024-12-09 10:22:10 -0500

    Day 09 - Part 2

--- day-09/src/main.rs
+++ day-09/src/main.rs
@@ -1,37 +1,20 @@
-use std::cmp::Ordering;
+use std::{cmp::Ordering, mem};
 
 const INPUT: &str = include_str!("../input.txt");
 
 fn main() {
     assert_eq!(6340197768906, filesystem_checksum(INPUT.trim()));
+    assert_eq!(6363913128533, filesystem_checksum_whole_file(INPUT.trim()));
 }
 
 fn filesystem_checksum(s: &str) -> u64 {
-    use Content::*;
-
-    let mut id = 0;
-
-    let mut disk = s
-        .chars()
-        .enumerate()
-        .map(|(i, c)| {
-            let len = c.to_digit(10).expect("Not a valid digit").into();
-
-            if i % 2 == 0 {
-                let c = File { len, id };
-                id += 1;
-                c
-            } else {
-                Free { len }
-            }
-        })
-        .collect::<Vec<_>>();
+    let mut disk = parse(s);
 
     loop {
         // dump(&disk);
 
         let mut src = match disk.pop() {
-            Some(Free { .. }) => {
+            Some(Content::Free { .. }) => {
                 // ignoring free space
                 continue;
             }
@@ -45,12 +28,12 @@ fn filesystem_checksum(s: &str) -> u64 {
             None => panic!("Ran out of contents"),
         };
 
-        let dst_idx = disk.iter().position(|c| matches!(c, Free { .. }));
+        let dst_idx = disk.iter().position(|c| matches!(c, Content::Free { .. }));
 
         // No more free space, done compacting
         let Some(dst_idx) = dst_idx else {
             disk.push(src);
-            break
+            break;
         };
         let dst = &mut disk[dst_idx];
 
@@ -75,15 +58,88 @@ fn filesystem_checksum(s: &str) -> u64 {
         }
     }
 
+    checksum(&disk)
+}
+
+fn filesystem_checksum_whole_file(s: &str) -> u64 {
+    let mut disk = parse(s);
+
+    let id = disk
+        .iter()
+        .rev()
+        .find_map(Content::to_file_id)
+        .expect("No files present");
+
+    for id in (0..=id).rev() {
+        // dump(&disk);
+
+        let src_idx = disk
+            .iter()
+            .rposition(|c| c.to_file_id() == Some(id))
+            .expect("Source file not found");
+        let src = &disk[src_idx];
+
+        let dst_idx = disk
+            .iter()
+            .position(|c| c.free_space().is_some_and(|f| f >= src.len()));
+        if let Some(dst_idx) = dst_idx {
+            // Only move to the left
+            if dst_idx < src_idx {
+                let src = disk[src_idx].take();
+
+                let dst = &mut disk[dst_idx];
+                let leftover = dst.split_at(src.len());
+                *dst = src;
+                disk.insert(dst_idx + 1, leftover);
+            }
+        }
+
+        // Coalesce free space
+        {
+            let mut i = 0;
+            while i + 1 < disk.len() {
+                if disk[i].is_free() && disk[i + 1].is_free() {
+                    let b = disk.remove(i + 1);
+                    disk[i].absorb(b);
+                } else {
+                    i += 1;
+                }
+            }
+        }
+    }
+
+    checksum(&disk)
+}
+
+fn parse(s: &str) -> Vec<Content> {
+    let mut id = 0;
+
+    s.chars()
+        .enumerate()
+        .map(|(i, c)| {
+            let len = c.to_digit(10).expect("Not a valid digit").into();
+
+            if i % 2 == 0 {
+                let c = Content::File { len, id };
+                id += 1;
+                c
+            } else {
+                Content::Free { len }
+            }
+        })
+        .collect()
+}
+
+fn checksum(disk: &[Content]) -> u64 {
     let mut block = 0;
 
     disk.iter()
         .map(|c| {
-            let File { len, id } = *c else {
-                panic!("Free space left")
+            let sum = match *c {
+                Content::File { len, id } => (0..len).map(|i| (i + block) * id).sum::<u64>(),
+                _ => 0,
             };
-            let sum = (0..len).map(|i| (i + block) * id).sum::<u64>();
-            block += len;
+            block += c.len();
             sum
         })
         .sum()
@@ -127,6 +183,39 @@ impl Content {
             }
         }
     }
+
+    fn take(&mut self) -> Self {
+        let empty = Content::Free { len: self.len() };
+        mem::replace(self, empty)
+    }
+
+    fn free_space(&self) -> Option<u64> {
+        match *self {
+            Content::File { .. } => None,
+            Content::Free { len } => Some(len),
+        }
+    }
+
+    fn to_file_id(&self) -> Option<u64> {
+        match *self {
+            Content::File { id, .. } => Some(id),
+            Content::Free { .. } => None,
+        }
+    }
+
+    fn is_free(&self) -> bool {
+        match self {
+            Content::File { .. } => false,
+            Content::Free { .. } => true,
+        }
+    }
+
+    fn absorb(&mut self, other: Content) {
+        match self {
+            Content::File { .. } => panic!("Can't absorb into a file"),
+            Content::Free { len } => *len += other.len(),
+        }
+    }
 }
 
 #[allow(unused)]
@@ -162,5 +251,10 @@ mod test {
     #[test]
     fn example() {
         assert_eq!(1928, filesystem_checksum(EXAMPLE));
+    }
+
+    #[test]
+    fn example_whole_file() {
+        assert_eq!(2858, filesystem_checksum_whole_file(EXAMPLE));
     }
 }
