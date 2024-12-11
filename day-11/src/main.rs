3b4fa10c06e69c9cd3214729cad775464d5d6fa6
Jake Goulding <jake.goulding@gmail.com>
2024-12-11 09:08:24 -0500

    Day 11 - Part 2

--- day-11/src/main.rs
+++ day-11/src/main.rs
@@ -1,9 +1,12 @@
 use itertools::Either;
+use std::collections::BTreeMap;
 
 const INPUT: &str = include_str!("../input.txt");
 
 fn main() {
     assert_eq!(203228, stones_after_blinks(INPUT, 25));
+    assert_eq!(203228, stones_after_blinks_memo(INPUT, 25));
+    assert_eq!(240884656550923, stones_after_blinks_memo(INPUT, 75));
 }
 
 fn stones_after_blinks(s: &str, n_blinks: usize) -> usize {
@@ -39,6 +42,52 @@ fn stones_after_blinks(s: &str, n_blinks: usize) -> usize {
     stones.len()
 }
 
+fn stones_after_blinks_memo(s: &str, n_blinks: usize) -> usize {
+    let stones = s
+        .split_ascii_whitespace()
+        .map(|n| n.parse::<u64>().expect("Invalid number"));
+
+    let mut memo = BTreeMap::new();
+
+    stones.map(|stone| delve(&mut memo, stone, n_blinks)).sum()
+}
+
+fn delve(memo: &mut BTreeMap<(u64, usize), usize>, stone: u64, depth: usize) -> usize {
+    let key = (stone, depth);
+
+    if let Some(&count) = memo.get(&key) {
+        return count;
+    }
+
+    let count = {
+        if depth == 0 {
+            1
+        } else if stone == 0 {
+            delve(memo, 1, depth - 1)
+        } else {
+            let n_digits = stone.ilog10() + 1;
+
+            if n_digits % 2 == 0 {
+                let factor = 10u64.pow(n_digits / 2);
+
+                let l = stone / factor;
+                let r = stone % factor;
+
+                let l = delve(memo, l, depth - 1);
+                let r = delve(memo, r, depth - 1);
+
+                l + r
+            } else {
+                delve(memo, stone * 2024, depth - 1)
+            }
+        }
+    };
+
+    memo.insert(key, count);
+
+    count
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -55,5 +104,16 @@ mod test {
     fn example_2() {
         assert_eq!(22, stones_after_blinks(EXAMPLE_2, 6));
         assert_eq!(55312, stones_after_blinks(EXAMPLE_2, 25));
+    }
+
+    #[test]
+    fn example_1_memo() {
+        assert_eq!(7, stones_after_blinks_memo(EXAMPLE_1, 1));
+    }
+
+    #[test]
+    fn example_2_memo() {
+        assert_eq!(22, stones_after_blinks_memo(EXAMPLE_2, 6));
+        assert_eq!(55312, stones_after_blinks_memo(EXAMPLE_2, 25));
     }
 }
