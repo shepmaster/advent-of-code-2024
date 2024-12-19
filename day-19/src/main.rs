295c5e72b8b8249803ca12ea0594a631a6756dca
Jake Goulding <jake.goulding@gmail.com>
2024-12-19 08:23:25 -0500

    Day 19 - Part 2

--- day-19/src/main.rs
+++ day-19/src/main.rs
@@ -4,38 +4,60 @@ const INPUT: &str = include_str!("../input.txt");
 
 fn main() {
     assert_eq!(317, possible_designs(INPUT));
+    assert_eq!(883443544805484, possible_design_counts(INPUT));
 }
 
 fn possible_designs(s: &str) -> usize {
-    let (avail, desired) = s.split_once("\n\n").expect("input malformed");
-    let avail = avail.split(',').map(|s| s.trim()).collect::<Vec<_>>();
+    let (avail, desired) = parse(s);
 
     let mut cache = Default::default();
 
     desired
-        .lines()
-        .filter(|desired| can_be_built_from(&mut cache, &avail, desired))
+        .filter(|desired| can_be_built_from(&mut cache, &avail, desired) != 0)
         .count()
 }
 
+fn possible_design_counts(s: &str) -> usize {
+    let (avail, desired) = parse(s);
+
+    let mut cache = Default::default();
+
+    desired
+        .map(|desired| can_be_built_from(&mut cache, &avail, desired))
+        .sum()
+}
+
+fn parse(s: &str) -> (Vec<&str>, impl Iterator<Item = &str>) {
+    let (avail, desired) = s.split_once("\n\n").expect("input malformed");
+
+    let avail = avail.split(',').map(|s| s.trim()).collect::<Vec<_>>();
+    let desired = desired.lines();
+
+    (avail, desired)
+}
+
 fn can_be_built_from<'a>(
-    cache: &mut BTreeMap<&'a str, bool>,
+    cache: &mut BTreeMap<&'a str, usize>,
     available: &[&str],
     desired: &'a str,
-) -> bool {
+) -> usize {
     if desired.is_empty() {
-        return true;
+        return 1;
     }
 
     if let Some(&cached) = cache.get(desired) {
         return cached;
     }
 
-    let can_be = available.iter().any(|&a| {
-        desired
-            .strip_prefix(a)
-            .is_some_and(|tail| can_be_built_from(cache, available, tail))
-    });
+    let can_be = available
+        .iter()
+        .map(|&a| {
+            desired
+                .strip_prefix(a)
+                .map(|tail| can_be_built_from(cache, available, tail))
+                .unwrap_or(0)
+        })
+        .sum();
 
     cache.insert(desired, can_be);
     can_be
@@ -50,5 +72,10 @@ mod test {
     #[test]
     fn example() {
         assert_eq!(6, possible_designs(EXAMPLE));
+    }
+
+    #[test]
+    fn example_counts() {
+        assert_eq!(16, possible_design_counts(EXAMPLE));
     }
 }
