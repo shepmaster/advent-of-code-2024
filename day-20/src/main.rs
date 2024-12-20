107ace2eaa76d86ecef183eacf69cae6ff3c344f
Jake Goulding <jake.goulding@gmail.com>
2024-12-20 11:43:18 -0500

    Day 20 - Part 1

---
+++ day-20/src/main.rs
@@ -0,0 +1,229 @@
+use std::collections::{BTreeMap, BTreeSet, BinaryHeap, btree_map::Entry};
+
+const INPUT: &str = include_str!("../input.txt");
+
+fn main() {
+    assert_eq!(1395, n_cheats(INPUT, 100));
+}
+
+fn n_cheats(s: &str, at_least_ps: usize) -> usize {
+    let mut start = None;
+    let mut end = None;
+    let mut walls = BTreeSet::new();
+    let mut max_x = 0;
+    let mut max_y = 0;
+
+    for (y, l) in s.lines().enumerate() {
+        for (x, c) in l.chars().enumerate() {
+            let coord = [x, y];
+
+            match c {
+                '.' => { /* Path */ }
+
+                '#' => {
+                    walls.insert(coord);
+                }
+
+                'S' => {
+                    assert!(start.is_none());
+                    start = Some(coord)
+                }
+
+                'E' => {
+                    assert!(end.is_none());
+                    end = Some(coord)
+                }
+
+                _ => panic!("Unknown map square {c}"),
+            }
+
+            max_x = x;
+        }
+
+        max_y = y;
+    }
+
+    let start = start.expect("start not found");
+    let end = end.expect("end not found");
+    let walls = Walls::new(walls, max_x, max_y);
+
+    // Idea: Walk backwards from the end to every place, tracking the
+    // distance, then walk forwards from the start using cheating to
+    // find how much we can save.
+
+    let distances = distances(&walls, end);
+    let saved = cheating_paths(&walls, start, end, distances);
+
+    saved
+        .into_iter()
+        .filter(|&(k, _)| k >= at_least_ps)
+        .map(|(_, v)| v)
+        .sum()
+}
+
+type Coord = [usize; 2];
+
+struct Walls {
+    coords: BTreeSet<Coord>,
+    max_x: usize,
+    max_y: usize,
+}
+
+impl Walls {
+    fn new(coords: BTreeSet<Coord>, max_x: usize, max_y: usize) -> Self {
+        Self {
+            coords,
+            max_x,
+            max_y,
+        }
+    }
+
+    fn valid(&self, coord: Coord) -> bool {
+        let Self {
+            ref coords,
+            max_x,
+            max_y,
+        } = *self;
+        let [x, y] = coord;
+        x <= max_x && y <= max_y && !coords.contains(&coord)
+    }
+}
+
+#[derive(Debug, PartialEq, Eq)]
+struct Candidate {
+    coord: Coord,
+    score: usize,
+}
+
+impl PartialOrd for Candidate {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for Candidate {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.score
+            .cmp(&other.score)
+            .reverse()
+            .then_with(|| self.coord.cmp(&other.coord))
+    }
+}
+
+type Distances = BTreeMap<Coord, usize>;
+
+fn distances(walls: &Walls, end: Coord) -> Distances {
+    let mut to_visit = BTreeSet::from_iter([Candidate {
+        coord: end,
+        score: 0,
+    }]);
+
+    let mut visited = BTreeMap::new();
+
+    while let Some(candidate) = to_visit.pop_first() {
+        match visited.entry(candidate.coord) {
+            Entry::Vacant(entry) => {
+                entry.insert(candidate.score);
+            }
+            Entry::Occupied(_) => continue,
+        }
+
+        let new_to_visit = neighbors(candidate.coord)
+            .filter(|&n| walls.valid(n))
+            .map(|coord| Candidate {
+                coord,
+                score: candidate.score + 1,
+            });
+        to_visit.extend(new_to_visit);
+    }
+
+    visited
+}
+
+fn cheating_paths(
+    walls: &Walls,
+    start: Coord,
+    end: Coord,
+    distances: Distances,
+) -> BTreeMap<usize, usize> {
+    let mut to_visit = BinaryHeap::from_iter([Candidate {
+        coord: start,
+        score: 0,
+    }]);
+
+    let mut visited = BTreeSet::new();
+    let mut shortcuts = BTreeMap::new();
+
+    while let Some(candidate) = to_visit.pop() {
+        if candidate.coord == end {
+            break;
+        }
+
+        let newly_visited = visited.insert(candidate.coord);
+        if !newly_visited {
+            continue;
+        }
+
+        // Check the cheating jumps
+        {
+            let start_distance = distances[&candidate.coord];
+
+            let cheating_coords = neighbors(candidate.coord)
+                .flat_map(neighbors)
+                .filter(|&n| walls.valid(n));
+
+            for cheat_coord in cheating_coords {
+                let saved = (|| {
+                    let cheat_distance = *distances.get(&cheat_coord)?;
+                    let delta = start_distance.checked_sub(cheat_distance)?;
+                    let saved = delta.checked_sub(2)?;
+                    (saved > 0).then_some(saved)
+                })();
+
+                if let Some(saved) = saved {
+                    *shortcuts.entry(saved).or_insert(0) += 1;
+                }
+            }
+        }
+
+        let new_to_visit = neighbors(candidate.coord)
+            .filter(|&n| walls.valid(n))
+            .map(|coord| Candidate {
+                coord,
+                score: candidate.score + 1,
+            });
+        to_visit.extend(new_to_visit);
+    }
+
+    shortcuts
+}
+
+fn neighbors(coord: Coord) -> impl Iterator<Item = Coord> {
+    let [x, y] = coord;
+
+    let u = y.checked_sub(1);
+    let r = x.checked_add(1);
+    let d = y.checked_add(1);
+    let l = x.checked_sub(1);
+
+    [
+        u.map(|y| [x, y]),
+        r.map(|x| [x, y]),
+        d.map(|y| [x, y]),
+        l.map(|x| [x, y]),
+    ]
+    .into_iter()
+    .flatten()
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    const EXAMPLE: &str = include_str!("../example.txt");
+
+    #[test]
+    fn example() {
+        assert_eq!(5, n_cheats(EXAMPLE, 20));
+    }
+}
