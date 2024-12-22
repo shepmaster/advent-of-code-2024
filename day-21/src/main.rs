8e70025420f6fd72364892e01910dad4a76f8964
Jake Goulding <jake.goulding@gmail.com>
2024-12-22 17:52:48 -0500

    Day 21 - Part 2

--- day-21/src/main.rs
+++ day-21/src/main.rs
@@ -2,14 +2,22 @@ use itertools::Itertools;
 use std::{
     cmp::Ordering,
     collections::{BTreeMap, BTreeSet, BinaryHeap},
-    fmt,
 };
 
 const INPUT: &str = include_str!("../input.txt");
 
 fn main() {
-    assert_eq!(163086, sum_of_complexities(INPUT));
+    assert_eq!(163086, sum_of_complexities(INPUT, 2));
+    assert_eq!(198466286401228, sum_of_complexities(INPUT, 25));
 }
+
+type Mapping<T> = BTreeMap<(T, T), Direction>;
+
+type Neighbors<T> = BTreeMap<T, Vec<T>>;
+
+type Paths<T> = BTreeMap<(T, T), BTreeSet<Vec<Direction>>>;
+type DigitPaths = Paths<KeypadDigit>;
+type DirectionPaths = Paths<KeypadDir>;
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord)]
 enum KeypadDigit {
@@ -50,7 +58,7 @@ impl KeypadDigit {
     // +---+---+---+
     //     | 0 | A |
     //     +---+---+
-    fn mapping() -> BTreeMap<(Self, Self), Direction> {
+    fn mapping() -> Mapping<Self> {
         use Direction::*;
         use KeypadDigit as K;
 
@@ -74,19 +82,11 @@ impl KeypadDigit {
             ((K::N3, K::A), D),
         ];
 
-        let other_direction = one_direction
-            .into_iter()
-            .map(|((a, b), d)| ((b, a), d.invert()));
-
-        one_direction.into_iter().chain(other_direction).collect()
+        complete_mapping(one_direction)
     }
 
-    fn neighbors() -> BTreeMap<Self, Vec<Self>> {
-        let mut neighbors = BTreeMap::new();
-        for (a, b) in Self::mapping().into_keys() {
-            neighbors.entry(a).or_insert_with(Vec::new).push(b);
-        }
-        neighbors
+    fn neighbors() -> Neighbors<Self> {
+        mapping_to_neighbors(Self::mapping())
     }
 }
 
@@ -107,7 +107,7 @@ impl KeypadDir {
     // +---+---+---+
     // | < | v | > |
     // +---+---+---+
-    fn mapping() -> BTreeMap<(Self, Self), Direction> {
+    fn mapping() -> Mapping<Self> {
         use Direction::*;
         use KeypadDir as K;
 
@@ -121,19 +121,11 @@ impl KeypadDir {
             ((K::A, K::R), D),
         ];
 
-        let other_direction = one_direction
-            .into_iter()
-            .map(|((a, b), d)| ((b, a), d.invert()));
-
-        one_direction.into_iter().chain(other_direction).collect()
+        complete_mapping(one_direction)
     }
 
-    fn neighbors() -> BTreeMap<Self, Vec<Self>> {
-        let mut neighbors = BTreeMap::new();
-        for (a, b) in Self::mapping().into_keys() {
-            neighbors.entry(a).or_insert_with(Vec::new).push(b);
-        }
-        neighbors
+    fn neighbors() -> Neighbors<Self> {
+        mapping_to_neighbors(Self::mapping())
     }
 
     #[allow(dead_code)]
@@ -169,6 +161,29 @@ impl From<Direction> for KeypadDir {
     }
 }
 
+fn complete_mapping<I, T>(one_direction: I) -> Mapping<T>
+where
+    I: IntoIterator<Item = ((T, T), Direction)> + Copy,
+    T: Ord,
+{
+    let other_direction = one_direction
+        .into_iter()
+        .map(|((a, b), d)| ((b, a), d.invert()));
+
+    one_direction.into_iter().chain(other_direction).collect()
+}
+
+fn mapping_to_neighbors<T>(mapping: Mapping<T>) -> Neighbors<T>
+where
+    T: Ord,
+{
+    let mut neighbors = BTreeMap::new();
+    for (a, b) in mapping.into_keys() {
+        neighbors.entry(a).or_insert_with(Vec::new).push(b);
+    }
+    neighbors
+}
+
 #[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord)]
 enum Direction {
     U,
@@ -190,127 +205,87 @@ impl Direction {
     }
 }
 
-fn sum_of_complexities(s: &str) -> usize {
+fn sum_of_complexities(s: &str, n_robots: usize) -> usize {
+    let digit_paths = all_paths_digits();
+    let dir_paths = all_paths_directions();
+
+    let mut the_cache = Default::default();
+
     s.lines()
         .map(|l| {
-            let buttons = l
-                .chars()
-                .map(|c| {
-                    use KeypadDigit::*;
-                    match c {
-                        '0' => N0,
-                        '1' => N1,
-                        '2' => N2,
-                        '3' => N3,
-                        '4' => N4,
-                        '5' => N5,
-                        '6' => N6,
-                        '7' => N7,
-                        '8' => N8,
-                        '9' => N9,
-                        'A' => A,
-                        _ => panic!("Unknown character {c}"),
-                    }
-                })
-                .collect();
+            let buttons = l.chars().map(|c| {
+                use KeypadDigit::*;
+                match c {
+                    '0' => N0,
+                    '1' => N1,
+                    '2' => N2,
+                    '3' => N3,
+                    '4' => N4,
+                    '5' => N5,
+                    '6' => N6,
+                    '7' => N7,
+                    '8' => N8,
+                    '9' => N9,
+                    'A' => A,
+                    _ => panic!("Unknown character {c}"),
+                }
+            });
 
             let value = l.trim().trim_end_matches('A').parse::<usize>().unwrap();
-            let minimum_buttons = minimum_button_presses(buttons);
+            let minimum_buttons = digit_to_n_direction_pushes(
+                &digit_paths,
+                &dir_paths,
+                buttons,
+                n_robots,
+                &mut the_cache,
+            );
 
             value * minimum_buttons
         })
         .sum()
 }
 
-fn minimum_button_presses(buttons: Vec<KeypadDigit>) -> usize {
-    let digit_paths = all_paths_digits();
-    let dir_paths = all_paths_directions();
-
-    let mut buttons_set = buttons_to_keypad_directions(vec![buttons], &digit_paths, KeypadDigit::A);
-
-    for _ in 0..2 {
-        buttons_set = buttons_to_keypad_directions(buttons_set, &dir_paths, KeypadDir::A);
+#[allow(dead_code)]
+fn dump_set(buttons_set: &[Vec<KeypadDir>]) {
+    for b in buttons_set {
+        eprintln!("{}", KeypadDir::as_string(b));
     }
-
-    buttons_set.iter().map(|b| b.len()).min().unwrap()
 }
 
-fn buttons_to_keypad_directions<T: Copy + Ord>(
-    buttons_set: Vec<Vec<T>>,
-    paths: &BTreeMap<(T, T), BTreeSet<Vec<Direction>>>,
-    start: T,
-) -> Vec<Vec<KeypadDir>> {
-    buttons_set
-        .into_iter()
-        .flat_map(|buttons| {
-            let mut current = start;
-            let mut my_paths = vec![vec![]];
-
-            for b in buttons {
-                // Just push the same button again
-                if current == b {
-                    for p in &mut my_paths {
-                        p.push(KeypadDir::A);
-                    }
-                    continue;
-                }
-
-                let new_paths = &paths[&(current, b)];
-
-                my_paths = my_paths
-                    .into_iter()
-                    .flat_map(|p| {
-                        new_paths.iter().map(move |np| {
-                            let mut p = p.clone();
-                            p.extend(np.iter().map(|&d| KeypadDir::from(d)));
-                            p.push(KeypadDir::A);
-                            p
-                        })
-                    })
-                    .collect();
-
-                current = b;
-            }
-
-            my_paths
-        })
-        .collect()
+fn pairs<I>(i: I) -> impl Iterator<Item = (I::Item, I::Item)>
+where
+    I: IntoIterator,
+    I::IntoIter: Clone,
+    I::Item: Clone,
+{
+    let i = i.into_iter();
+    i.clone().cartesian_product(i)
 }
 
-fn all_paths_digits() -> BTreeMap<(KeypadDigit, KeypadDigit), BTreeSet<Vec<Direction>>> {
+fn all_paths_digits() -> DigitPaths {
     let mapping = KeypadDigit::mapping();
     let neighbors = KeypadDigit::neighbors();
 
-    let pairs = KeypadDigit::ALL
-        .into_iter()
-        .cartesian_product(KeypadDigit::ALL);
-
-    all_paths(mapping, neighbors, pairs)
+    all_paths(mapping, neighbors, pairs(KeypadDigit::ALL))
 }
 
-fn all_paths_directions() -> BTreeMap<(KeypadDir, KeypadDir), BTreeSet<Vec<Direction>>> {
+fn all_paths_directions() -> DirectionPaths {
     let mapping = KeypadDir::mapping();
     let neighbors = KeypadDir::neighbors();
 
-    let pairs = KeypadDir::ALL.into_iter().cartesian_product(KeypadDir::ALL);
-
-    all_paths(mapping, neighbors, pairs)
+    all_paths(mapping, neighbors, pairs(KeypadDir::ALL))
 }
 
-fn all_paths<T: fmt::Debug + Copy + Ord>(
-    mapping: BTreeMap<(T, T), Direction>,
-    neighbors: BTreeMap<T, Vec<T>>,
+fn all_paths<T: Copy + Ord>(
+    mapping: Mapping<T>,
+    neighbors: Neighbors<T>,
     pairs: impl Iterator<Item = (T, T)>,
-) -> BTreeMap<(T, T), BTreeSet<Vec<Direction>>> {
+) -> Paths<T> {
     let mut commands = BTreeMap::new();
 
     for (start, end) in pairs {
         // Could be smarter: if A,B are already known and we are
         // looking for B,A could invert the steps, but eh.
-
-        if start == end {
-            continue;
-        }
 
         let mut to_visit = BinaryHeap::from_iter([Candidate {
             coord: start,
@@ -419,6 +394,94 @@ impl<T: Ord> PartialOrd for Candidate<T> {
     }
 }
 
+fn digit_to_n_direction_pushes(
+    digit_paths: &DigitPaths,
+    dir_paths: &DirectionPaths,
+    targets: impl IntoIterator<Item = KeypadDigit>,
+    depth: usize,
+    cache: &mut Cache,
+) -> usize {
+    let mut current = KeypadDigit::A;
+
+    targets
+        .into_iter()
+        .map(|target| {
+            let paths = &digit_paths[&(current, target)];
+
+            let v = paths
+                .iter()
+                .map(|path| {
+                    if depth > 0 {
+                        let mut keypad_path = path.iter().map(|&p| p.into()).chain([KeypadDir::A]);
+                        direction_to_n_direction_pushes(
+                            dir_paths,
+                            &mut keypad_path,
+                            depth - 1,
+                            cache,
+                        )
+                    } else {
+                        path.len() + 1 // Also press A
+                    }
+                })
+                .min()
+                .unwrap();
+
+            current = target;
+
+            v
+        })
+        .sum()
+}
+
+type CacheKey = (KeypadDir, KeypadDir, usize);
+type Cache = BTreeMap<CacheKey, usize>;
+
+fn direction_to_n_direction_pushes(
+    dir_paths: &DirectionPaths,
+    targets: &mut dyn Iterator<Item = KeypadDir>,
+    depth: usize,
+    cache: &mut Cache,
+) -> usize {
+    let mut current = KeypadDir::A;
+
+    targets
+        .into_iter()
+        .map(|target| {
+            let cache_key = (current, target, depth);
+
+            if let Some(&v) = cache.get(&cache_key) {
+                current = target;
+                return v;
+            }
+
+            let v = dir_paths[&(current, target)]
+                .iter()
+                .map(|path| {
+                    if depth > 0 {
+                        let mut keypad_path = path.iter().map(|&p| p.into()).chain([KeypadDir::A]);
+                        direction_to_n_direction_pushes(
+                            dir_paths,
+                            &mut keypad_path,
+                            depth - 1,
+                            cache,
+                        )
+                    } else {
+                        // The human can directly press buttons
+                        path.len() + 1 // Also press A
+                    }
+                })
+                .min()
+                .unwrap();
+
+            cache.insert(cache_key, v);
+
+            current = target;
+
+            v
+        })
+        .sum()
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -427,7 +490,6 @@ mod test {
 
     #[test]
     fn example() {
-        assert_eq!(126384, sum_of_complexities(EXAMPLE));
-        panic!()
+        assert_eq!(126384, sum_of_complexities(EXAMPLE, 2));
     }
 }
