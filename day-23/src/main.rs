30f502a67e5088512954217a018755202a4f18df
Jake Goulding <jake.goulding@gmail.com>
2024-12-23 15:13:25 -0500

    Day 23 - Part 2

--- day-23/src/main.rs
+++ day-23/src/main.rs
@@ -4,18 +4,11 @@ const INPUT: &str = include_str!("../input.txt");
 
 fn main() {
     assert_eq!(1314, n_sets_where_starts_with_t(INPUT));
+    assert_eq!("bg,bu,ce,ga,hw,jw,nf,nt,ox,tj,uu,vk,wp", biggest_set(INPUT));
 }
 
 fn n_sets_where_starts_with_t(s: &str) -> usize {
-    let mut connections = BTreeMap::new();
-    let mut add_connection = |a, b| {
-        connections.entry(a).or_insert_with(BTreeSet::new).insert(b);
-    };
-    for l in s.lines() {
-        let (l, r) = l.split_once('-').expect("malformed line");
-        add_connection(l, r);
-        add_connection(r, l);
-    }
+    let connections = parse(s);
 
     connections
         .keys()
@@ -24,6 +17,49 @@ fn n_sets_where_starts_with_t(s: &str) -> usize {
         .into_iter()
         .filter(|path| path.iter().any(|&n| n.starts_with('t')))
         .count()
+}
+
+fn biggest_set(s: &str) -> String {
+    let connections = parse(s);
+
+    let p_set = connections.keys().copied().collect();
+    let mut maximal_cliques = Default::default();
+
+    bron_kerbosch(
+        &connections,
+        Default::default(),
+        p_set,
+        Default::default(),
+        &mut maximal_cliques,
+    );
+
+    let max_size = maximal_cliques
+        .iter()
+        .map(|c| c.len())
+        .max()
+        .expect("No maximum size found");
+
+    let max_clique = maximal_cliques
+        .into_iter()
+        .find(|c| c.len() == max_size)
+        .expect("No maximum clique found");
+
+    max_clique.into_iter().collect::<Vec<_>>().join(",")
+}
+
+fn parse(s: &str) -> BTreeMap<&str, BTreeSet<&str>> {
+    let mut connections = BTreeMap::new();
+    let mut add_connection = |a, b| {
+        connections.entry(a).or_insert_with(BTreeSet::new).insert(b);
+    };
+
+    for l in s.lines() {
+        let (l, r) = l.split_once('-').expect("malformed line");
+        add_connection(l, r);
+        add_connection(r, l);
+    }
+
+    connections
 }
 
 fn reachable<'a>(
@@ -52,6 +88,37 @@ fn reachable<'a>(
         .collect()
 }
 
+// https://en.wikipedia.org/wiki/Bron–Kerbosch_algorithm#Without_pivoting
+fn bron_kerbosch<'a>(
+    connections: &BTreeMap<&'a str, BTreeSet<&'a str>>,
+    r_set: BTreeSet<&'a str>,
+    mut p_set: BTreeSet<&'a str>,
+    mut x_set: BTreeSet<&'a str>,
+    output: &mut BTreeSet<BTreeSet<&'a str>>,
+) {
+    if p_set.is_empty() && x_set.is_empty() {
+        output.insert(r_set);
+        return;
+    }
+
+    while let Some(v) = p_set.pop_first() {
+        let neighbors = &connections[v];
+
+        let next_r_set = {
+            let mut r_set = r_set.clone();
+            r_set.insert(v);
+            r_set
+        };
+        let next_p_set = p_set.intersection(neighbors).copied().collect();
+        let next_x_set = x_set.intersection(neighbors).copied().collect();
+
+        bron_kerbosch(connections, next_r_set, next_p_set, next_x_set, output);
+
+        // v has already been removed from p_set
+        x_set.insert(v);
+    }
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -61,5 +128,10 @@ mod test {
     #[test]
     fn example() {
         assert_eq!(7, n_sets_where_starts_with_t(EXAMPLE));
+    }
+
+    #[test]
+    fn example_biggest() {
+        assert_eq!("co,de,ka,ta", biggest_set(EXAMPLE));
     }
 }
